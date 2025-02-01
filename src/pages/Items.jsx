2cf7198031d62993dd44456c839ea0b0b16a518b
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-01 22:27:19 +0100

    feat: added loading and error message & printed items cards - src/pages/Items.jsx

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -2,24 +2,34 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from '../features/exampleSlice';
 import axios from '../api/thebazaar-source';
+import ItemCard from '../components/ItemCard';
 
 function Items() {
 
    const [items, setItems] = useState([]);
+   const [effects, setEffects] = useState([]);
    const [loading, setLoading] = useState(true);
-   const [error, setError] = useState(null);
+   const [loadingError, setLoadingError] = useState(null);
 
    async function fetchItems() {
 
       try {
 
+         setLoading(true);
+
          const response = await axios.get('/items');
 
-         setItems(response.data);
+         const result = await response.data.result;
+
+         console.log('result', result);
+
+         setItems(result.items);
+
+         setEffects(result.effects);
 
       } catch (err) {
 
-         setError(err.message);
+         setLoadingError(err.message);
 
       } finally {
 
@@ -35,14 +45,43 @@ function Items() {
 
    }, []);
 
-   console.log(items);
-   
+   console.log('items', items);
+
+   console.log('effects', effects);
 
    return (
 
       <div className="container mx-auto my-5">
 
          <h1 className="text-2xl font-bold">Items Page</h1>
+
+         {
+            loading ?
+
+               <div className='text-center mx-auto my-5'>Loading data. Please wait...</div>
+
+               : null
+         }
+
+         {
+            loadingError ?
+
+               <div className='text-center mx-auto my-5'>Loading error occurred. Please retry.</div>
+
+               : null
+         }
+
+         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 my-5">
+
+            {
+               items.map((item) => (
+
+                  <ItemCard key={item.id} item={item} effects={effects}/>
+
+               ))
+            }
+
+         </div>
 
       </div>
 
