05432e1d14d11f9bd45726c3c76cdc343ea2a4be
Emilio Gallo <emilio.gallo17@gmail.com>
2025-02-01 20:19:17 +0100

    feat: created structure for api call to DB - src/pages/Items.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../features/exampleSlice';
-import axios from '../api/axios';
+import React from 'react';
 
 function Home() {
-
-   const [data, setData] = useState([]);
-
-   const count = useSelector(state => state.example.value);
-   const dispatch = useDispatch();
-
-   useEffect(() => {
-
-      axios.get('/posts')
-         .then(response => setData(response.data))
-         .catch(error => console.error(error));
-         
-   }, []);
 
    return (
 
       <div className="container mx-auto my-5">
 
          <h1 className="text-2xl font-bold">Home Page</h1>
-
-         <div>
-
-            <p>Count: {count}</p>
-
-            <button onClick={() => dispatch(increment())}>Increment</button>
-
-            <button onClick={() => dispatch(decrement())}>Decrement</button>
-
-         </div>
-
-         <ul>
-
-            {
-               data.map(post => (
-
-                  <li key={post.id}>- {post.title}</li>
-
-               ))
-            }
-
-         </ul>
 
       </div>
 
--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,6 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../features/exampleSlice';
+import axios from '../api/thebazaar-source';
 
 function Items() {
+
+   const [items, setItems] = useState([]);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(null);
+
+   async function fetchItems() {
+
+      try {
+
+         const response = await axios.get('/items');
+
+         setItems(response.data);
+
+      } catch (err) {
+
+         setError(err.message);
+
+      } finally {
+
+         setLoading(false);
+
+      };
+
+   };
+
+   useEffect(() => {
+
+      fetchItems();
+
+   }, []);
+
+   console.log(items);
+   
 
    return (
 
